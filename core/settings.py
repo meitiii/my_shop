b2cff87015679f3104208ebc7935b3cb4d5d306b
MEHDI <mahdifekri1384414@gmail.com>
2026-08-03 18:23:40 +0330

    feat:add django-filter(search,ordering,filtering) to product app

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'reviews',
     #DRF
     'rest_framework',
+    'django-filter',
 ]
 
 MIDDLEWARE = [
@@ -136,7 +137,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 from datetime import timedelta
--- products/views.py
+++ products/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from rest_framework.permissions import AllowAny
 from .models import Products,Category,ProductVariant,ProductImage
 from .serializers import ProductSerializer,CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AllowAny]
+ 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Products.objects.all().order_by('-created_at')
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
+
+    
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    filterset_fields = ['category','brand']
+    search_fields = ['name','description']
+    ordering_fields = ['created_at','name']
+
 
