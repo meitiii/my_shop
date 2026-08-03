b2fd42b9899cc612a626a3c065957bef0cd4cf8a
MEHDI <mahdifekri1384414@gmail.com>
2026-08-04 00:19:05 +0330

    fix: add payment model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from orders.models import OrderItem,Order
 
-# Create your models here.
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'در انتظار پرداخت'),
+        ('successful', 'موفق'),
+        ('failed', 'ناموفق'),
+    )
+
+    order = models.OneToOneField(Order,on_delete=models.CASCADE,related_name='payment')
+    amount = models.DecimalField(max_digits=12,decimal_places=2)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default='pending')
+
+    authority = models.CharField(max_length=100,blank=True,null=True)
+    ref_id = models.CharField(max_length=100,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment {self.id} - Order {self.order.id} - {self.status}"
