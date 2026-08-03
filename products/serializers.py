4b364ed86a0285880fd43e62be805188b7f4b62d
MEHDI <mahdifekri1384414@gmail.com>
2026-08-03 14:37:28 +0330

    feat:add Products Serializers (nested serializer and other)

---
+++ products/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Category,ProductImage,ProductVariant,Products
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name','slug','parent']
+
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id','image','alt_text']
+
+
+
+class ProductVariantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductVariant
+        fields = ['id','color','size','price','stock']
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True,read_only=True)
+    variants = ProductVariantSerializer(many=True,read_only= True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Products
+        fields = ['id','name','slug','description',
+                  'category','brand','created_at',
+                  'updated_at','images','variants']
