1771f5a43f614e2ae0df9529f234819810fc5bb9
MEHDI <mahdifekri1384414@gmail.com>
2026-08-10 23:21:46 +0330

    feat:add change and forgot password serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import Address
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth import password_validation
+from django.utils.encoding import force_str
+from django.contrib.auth.tokens import default_token_generator
 User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):  
     password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
@@ -31,6 +36,36 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         data['is_staff'] = self.user.is_staff
         data['username'] = self.user.username
         return data
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+        read_only_fields = ('user','created_at')
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required = True,write_only=True)
+    new_password = serializers.CharField(required =True,write_only = True,validators=[password_validation.validate_password])
+    new_password_confirm = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password_confirm']:
+            raise serializers.ValidationError({"new_password":"The new passwords do not match."})
+        return attrs
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    unidb64 = serializers.CharField(required= True)
+    token = serializers.CharField(required=True)
+    new_password = serializers.CharField(required =True,write_only = True,validators=[password_validation.validate_password])
+
+
+
+
 
 
 
