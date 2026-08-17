c40077e864bd4620558fe68f2a2c6fc2637d0df8
MEHDI <mahdifekri1384414@gmail.com>
2026-08-18 00:17:00 +0330

    fix: add cartpage for when user logout

--- cart/views.py
+++ cart/views.py
@@ -6,51 +6,139 @@ from rest_framework import viewsets,status
 from products.models import ProductVariant
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
-
+from rest_framework.decorators import action
 class CartViewSet(viewsets.ModelViewSet):
     serializer_class = CartItemSerializer
     permission_classes = [IsAuthenticated]
 
-
-
     def get_queryset(self):
-        cart,created = Cart.objects.get_or_create(user=self.request.user)
-        return CartItem.objects.filter(cart = cart)
-    
+        cart, created = Cart.objects.get_or_create(
+            user=self.request.user
+        )
+
+        return CartItem.objects.filter(cart=cart)
+
     def create(self, request, *args, **kwargs):
-        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart, created = Cart.objects.get_or_create(
+            user=request.user
+        )
+
         variant_id = request.data.get('variant')
         quantity = int(request.data.get('quantity', 1))
 
-        variant = get_object_or_404(ProductVariant, id=variant_id)
+        variant = get_object_or_404(
+            ProductVariant,
+            id=variant_id
+        )
 
         if variant.stock < quantity:
-            return Response({'error': 'Not enough stock available.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'error': 'Not enough stock available.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-        cart_item, is_created = CartItem.objects.get_or_create(cart=cart, variant=variant)
-        
+        cart_item, is_created = CartItem.objects.get_or_create(
+            cart=cart,
+            variant=variant
+        )
+
         if not is_created:
+
             if variant.stock < (cart_item.quantity + quantity):
-                return Response({'error': 'Cannot add more. Not enough stock.'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {'error': 'Cannot add more. Not enough stock.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
             cart_item.quantity += quantity
             cart_item.save()
+
         else:
             cart_item.quantity = quantity
             cart_item.save()
 
         serializer = self.get_serializer(cart_item)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', True)
+
         instance = self.get_object()
-        new_quantity = int(request.data.get('quantity', instance.quantity))
+
+        new_quantity = int(
+            request.data.get(
+                'quantity',
+                instance.quantity
+            )
+        )
 
         if instance.variant.stock < new_quantity:
-            return Response({'error': 'Requested quantity exceeds stock.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'error': 'Requested quantity exceeds stock.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         instance.quantity = new_quantity
         instance.save()
-        
+
         serializer = self.get_serializer(instance)
+
         return Response(serializer.data)
+
+    @action(
+        detail=False,
+        methods=['post'],
+        permission_classes=[IsAuthenticated]
+    )
+    def sync(self, request):
+
+        cart, _ = Cart.objects.get_or_create(
+            user=request.user
+        )
+
+        local_items = request.data.get('items', [])
+
+        for item in local_items:
+
+            variant_id = item.get('variant')
+            quantity = int(item.get('quantity', 1))
+
+            try:
+                variant = ProductVariant.objects.get(
+                    id=variant_id
+                )
+            except ProductVariant.DoesNotExist:
+                continue
+
+            if variant.stock <= 0:
+                continue
+
+            cart_item, created = CartItem.objects.get_or_create(
+                cart=cart,
+                variant=variant
+            )
+
+            if created:
+                cart_item.quantity = min(
+                    quantity,
+                    variant.stock
+                )
+
+            else:
+                new_quantity = cart_item.quantity + quantity
+
+                cart_item.quantity = min(
+                    new_quantity,
+                    variant.stock
+                )
+
+            cart_item.save()
+
+        return Response(
+            {'status': 'Cart synced successfully'},
+            status=status.HTTP_200_OK
+        )
--- payments/views.py
+++ payments/views.py
@@ -1,54 +1,58 @@
-from django.shortcuts import render
-from .models import Payment
-from orders.models import Order,OrderItem
-from rest_framework import views,status
+from rest_framework import views, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny,IsAuthenticated
-from django.shortcuts import get_list_or_404,get_object_or_404,redirect
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import get_object_or_404, redirect
+from .models import Payment
+from orders.models import Order
 import uuid
-
 
 class PaymentRequestView(views.APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self,request,order_id):
-        order = get_object_or_404(Order,id = order_id,user=request.user)
-        if order.status!='pending':
-            return Response({"error":"This order has already paid for or is invalid."},status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id, user=request.user)
+        if order.status != 'pending':
+            return Response({"error": "This order has already been paid or is invalid."}, status=status.HTTP_400_BAD_REQUEST)
 
         authority = str(uuid.uuid4())
 
-        payment,created = Payment.objects.get_or_create(order=order,defaults=
-                                                        {'amount':order.total_price,
-                                                         'authority':authority})
+        payment, created = Payment.objects.get_or_create(
+            order=order,
+            defaults={'amount': order.total_price, 'authority': authority}
+        )
         if not created:
             payment.authority = authority
             payment.save()
 
-        verify_url = f"http://127.0.0.1:8000/api/payments/verify/?authority={payment.authority}"
+        verify_path = f"/api/payments/verify/?authority={payment.authority}"
+        verify_url = request.build_absolute_uri(verify_path)
 
         return Response({
-            "message":"You will be redirected to the payment gateway.",
-            "payment_url":verify_url,
-            "authority":payment.authority
+            "message": "You will be redirected to the payment gateway.",
+            "payment_url": verify_url,
+            "authority": payment.authority
         })
 
 
 class PaymentVerifyView(views.APIView):
     permission_classes = [AllowAny]
 
-    def get(self,request):
+    def get(self, request):
         authority = request.query_params.get('authority')
-        FRONTEND_URL = "http://localhost:5173/payment/verify"
+        FRONTEND_URL = "http://localhost:8080/payment/verify" 
+        
         if not authority:
-            return Response({"error":"The authority parameter is required."},status=status.HTTP_400_BAD_REQUEST)
+            return redirect(f"{FRONTEND_URL}?status=failed&error=Authority parameter is missing.")
 
-        payment = get_object_or_404(Payment,authority=authority)
+        payment = get_object_or_404(Payment, authority=authority)
 
-        if payment.status=='successful':
+        if payment.status == 'successful':
             return redirect(f"{FRONTEND_URL}?status=success&message=This payment has already been approved.")
+        
+        # شبیه‌سازی پرداخت موفق
         payment.status = 'successful'
-        payment.ref_id = str(uuid.uuid4().hex)[:10]
+        # 👈 کد پیگیری رو شبیه کدهای بانکی (حروف بزرگ) کردم
+        payment.ref_id = str(uuid.uuid4().hex)[:10].upper() 
         payment.save()
 
         order = payment.order
@@ -56,4 +60,3 @@ class PaymentVerifyView(views.APIView):
         order.save()
 
         return redirect(f"{FRONTEND_URL}?status=success&ref_id={payment.ref_id}&order_id={order.id}")
-        
